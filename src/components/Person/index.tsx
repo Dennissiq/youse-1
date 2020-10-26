0b9f464007538a7d83cd3129e5d59c7eb793081d
Paulo Henrique Silveira <paulohsilveira@outlook.com>
2020-10-26 17:51:00 -0300

    Improving style

--- src/components/Person/index.tsx
+++ src/components/Person/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Person from '../../interfaces/Person'
 import { Div, Article, H3, Span } from './styles'
-import styled from 'styled-components'
 
-export default ({ item, modifier }: { item: Person, modifier: number }) => 
+export default ({ item, modifier }: { item: Person; modifier: number }) => (
   <Div modifier={modifier}>
     <Article>
       <H3>{item.title}</H3>
       <Span>{item.description}</Span>
     </Article>
   </Div>
+)
--- src/components/Person/styles.ts
+++ src/components/Person/styles.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 
 export const Div = styled.div`
   padding: 30px 20px;
-  background-color: ${(props: { modifier: number }) => props.modifier % 2 === 0 ? '#6a418c' : '#7d3caf'};
+  background-color: ${(props: { modifier: number }) =>
+    props.modifier % 2 === 0 ? '#6a418c' : '#7d3caf'};
 `
 
 export const Article = styled.article`
