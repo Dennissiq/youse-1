22f9479a5e81889d8cd59a1596151bc378dbd2f3
Paulo Henrique Silveira <paulohsilveira@outlook.com>
2020-10-28 10:29:25 -0300

    Adding list test

--- src/components/__tests__/List.test.js
+++ src/components/__tests__/List.test.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import List from '../List'
+import Person from '../Person'
+import { filtredItems } from '../__mocks__'
 
-it('renders correctly', () => {
-  const filtredItems = [
-    {
-      description: 'Ahmad31@yahoo.com',
-      id: 0,
-      title: 'Estefania Kunze',
-    },
-  ]
+Enzyme.configure({ adapter: new Adapter() })
 
-  const tree = renderer
-    .create(<List isLoading={false} filtredItems={filtredItems} />)
-    .toJSON()
-  expect(tree).toMatchSnapshot()
+describe('<List />', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<List isLoading={false} filtredItems={filtredItems} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('lists', () => {
+    const wrapper = shallow(<List filtredItems={filtredItems} />)
+
+    expect(wrapper.find(Person).length).toEqual(21)
+  })
 })
